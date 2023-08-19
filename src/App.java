6f6b37cb4fd2eeb1a0737a66261ee5c4225c7fba
andersonfajardo <anderson.fajardo@msn.com>
2023-08-19 17:56:30 -0300

    commit 3 - tarefa 1 e 2 concluída

--- src/App.java
+++ src/App.java
@@ -1,11 +1,12 @@
 public class App {
     public static void main(String[] args) {
-        Consultas consultas = new Consultas();
+        RegistroDoTempoRepository repository = new RegistroDoTempoRepository();
+        Consultas consultas = new Consultas(repository);
         consultas.carregaDados();
-        System.out.println("Dia em que mais choveu no ano de 1980: ");
-        System.out.println(consultas.diaQueMaisChoveuNoAno(1980));
-        System.out.println("Datas em que choveu mais de 90 milimetros");
-        consultas.datasEmQueChouveuMaisDe(90)
+        System.out.println("Dia em que mais choveu no ano de 1970: ");
+        System.out.println(consultas.diaQueMaisChoveuNoAno(1970));
+        System.out.println("Datas em que choveu mais de 80 milimetros");
+        consultas.datasEmQueChouveuMaisDe(80)
             .forEach(System.out::println);
     }
 }
